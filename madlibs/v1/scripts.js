ae22c4eac2b0c6a3c23150b88fdd480c57e2851a
jcducddes157a <jrcrooks@ucdavis.edu>
2026-02-05 01:47:19 -0800

    idfk

--- madlibs/v1/scripts.js
+++ madlibs/v1/scripts.js
@@ -19,91 +19,185 @@
 
     const aliensRealForm = document.querySelector('#aliens-real')
     const aliensRealImgs = document.querySelector('#aliens-real-img')
+    const realSections = document.querySelectorAll('#aliens-real section')
 
     const aliensFakeForm = document.querySelector('#aliens-fake')
     const aliensFakeImgs = document.querySelector('#aliens-fake-img')
+    const fakeSections = document.querySelectorAll('#aliens-fake section')
 
     const aliensYes = document.querySelector('#yes-aliens')
     const aliensNo = document.querySelector('#no-aliens')
 
-    const prevButton = document.querySelector('.prev')
-    const nextButton = document.querySelector('.next')
     const submitButton = document.querySelector('#submit')
 
     let progressBarPercent = document.querySelector('#percent-complete')
     let progressBarVisual = document.querySelector('#progress-bar')
 
-    let pageNum
-
+   // Getting Rid of Content for Initial Prompt in case of CSS Failure
+    aliensFakeForm.style.display = 'none'
+    aliensFakeImgs.style.display = 'none'
+    aliensRealForm.style.display = 'none'
+    aliensRealImgs.style.display = 'none'
     submitButton.style.display = "none"
 
-    // Makes the first text box disappear after the initial question
-    initButtonSelect.addEventListener('click', function(){
-        if (aliensYes.checked) {
-            initFormSelect.style.display = 'none'
-            initImgs.style.display = 'none'
+    // Variables that track pathing and sections
+    let currentPage = 0
+    let path // Empty string to be set to "real" or 'fake'
+    let sections = []
+    let totalSections = 0
 
-            aliensRealForm.style.display = 'block'
-
-            progressBarPercent.innerHTML = '20'
-            progressBarVisual.style.width = '20%'
-        } else if (aliensNo.checked) {
-            initFormSelect.style.display = 'none'
-            initImgs.style.display = 'none'
-
-            aliensFakeForm.style.display = 'block'
-
-            progressBarPercent.innerHTML = '20'
-            progressBarVisual.style.width = '20%'
-        } else {
-            alert("Please select an option.")
+    // Hides Sections not labelled as class Showing
+    const allSections = document.querySelectorAll('section')
+    for (let i = 0; i < allSections.length; i++) {
+        if (allSections[i].className === 'hidden') {
+            allSections[i].style.display = 'none'
         }
-    })
+    }
 
-    // Back Button for Text Pages
-    // Back button not working for "aliensFake" pages; why???
-    prevButton.addEventListener('click', function(){
-        aliensFakeForm.style.display = "none"
-        aliensFakeImgs.style.display = 'none'
+    // Variables for Button Tracking
+    const nextButton = document.querySelectorAll('.next')
+    const prevButton = document.querySelectorAll('.prev')
 
-        aliensRealForm.style.display = "none"
-        aliensRealImgs.style.display = 'none'
-
-        initFormSelect.style.display = "block"
-        initImgs.style.display = 'contents'
-
-        progressBarPercent.innerHTML = '0'
-        progressBarVisual.style.width = '0%'
-    })
-
-
-    // Checks everything on submit
-    fullFormSelect.addEventListener('submit', function(){
+    // Function for Path Determination
+    function determiner() {
         if (aliensYes.checked) {
-            const allYesTextInputs = document.querySelectorAll('#aliens-real input[type="text"]')
+            path = 'real'
+            sections = realSections
+            totalSections = realSections.length
+            initFormSelect.style.display = 'none'
+            aliensRealForm.style.display = 'block'
+            progressBarPercent.innerHTML = '20'
+            progressBarVisual.style.width = '20%'
+        }
+        else if (aliensNo.checked) {
+            path = 'fake'
+            sections = fakeSections
+            totalSections = fakeSections.length
+            initFormSelect.style.display = 'none'
+            aliensFakeForm.style.display = 'block'
+            progressBarPercent.innerHTML = '20'
+            progressBarVisual.style.width = '20%'
+        }
+        else {
+            alert('Please select an option.')
+        }
+    }
 
-            // Returns an error if user hasn't filled out form fully
-            // If no error, user gets completed madlib
-            for (var i = 0; i <= allYesTextInputs.length; i++) {
-                if (allYesTextInputs[i] == '') {
-                    alert('Error! Please fill out all forms.')
-                } else {
-                    
+    // Function for Validation of Current Section Inputs
+    function checkCurrentSection() {
+        const currentSection = section[currentPage - 1]
+        const currentInputs = currentSection.querySelectorAll('input[type="text"]')
+
+        for (let inputs of currentInputs) {
+            if (inputs.value === '') {
+                alert('Please fill out all text boxes.')
+            }
+        }
+    }
+
+    function updateUI() {
+        alert(`Current Page: ${currentPage}`)
+        alert(`Sections: ${totalSections}`)
+
+        //Reset 
+        if (currentPage === 0) {
+            if (path === 'real') {
+            aliensRealForm.style.display = 'none'
+        } else if (path === 'fake') {
+            aliensFakeForm.style.display = 'none'
+        }
+        initFormSelect.style.display = 'block'
+        submitButton.style.display = 'none'
+        aliensNo.checked = false
+        aliensYes.checked = false
+        path = ''
+        sections = []
+        totalSections = 0
+        } else {
+            for (let i = 0; i < sections.length; i++) {
+                switch (sections[i]) {
+                    case i === currentPage - 1:
+                        sections[i].style.display = 'block'
+                        break
+                    default: 
+                        sections[i].style.display = 'hidden'
+                        break
                 }
             }
+        }
+    }
 
-            let yesText
+    // Next Button Function
+    for (let i = 0; i < nextButton.length; i++) {
+        nextButton[i].addEventListener('click', function () {
+        if (currentPage === 0) {
+            determiner()
+        } else {
+            checkCurrentSection()
+        }
 
-            aliensRealImgs.style.display = 'contents'
+        if (currentPage < totalSections) {
+            currentPage++
+            updateUI()
+        } else if (currentPage === totalSections) {
+            nextButton[currentPage].style.display = 'none'
+            submitButton.style.display = 'block'
+        }
 
-            document.querySelector('#madlib-real').innerHTML = yesText
+        })
+    }
+
+    //Prev Button Function
+    for (let i = 0; i < prevButton.length; i++) {
+        prevButton[i].addEventListener('click', function() {
+            if (currentPage > 0) {
+                currentPage--
+                updateUI()
+            }
+        })
+    }
+   
+    // Submit Function
+    fullFormSelect.addEventListener('submit', function(){
+        if (aliensYes.checked) {
+            submitButton.style.display = 'contents'
+           yesMadLib()
         } else if (aliensNo.checked) {
-            
-            let noText = ``
-
-            aliensFakeImgs.style.display = 'contents'
-
-            document.querySelector('#madlib-fake').innerHTML = noText
+            noMadLib()
         }
     })
+
+    function yesMadLib() {
+        const allYesTextInputs = document.querySelectorAll('#aliens-real input[type="text"]')
+
+        let yesText
+
+        // Returns an error if user hasn't filled out form fully
+        // If no error, user gets completed madlib
+        for (let i = 0; i < allYesTextInputs.length; i++) {
+            if (allYesTextInputs[i].value == '') {
+                alert ('ERROR: Please enter all text boxes.')
+            } else {
+                aliensRealForm.style.display = 'none'
+                aliensRealImgs.style.display = 'contents'
+                document.querySelector('#madlib-real').innerHTML = yesText
+            }
+        }
+    }
+
+    function noMadLib() {
+        const allNoTextInputs = document.querySelectorAll('#aliens-fake input[type="text"]')
+
+        let noText
+
+        for (let i = 0; i < allNoTextInputs.length; i++) {
+            if (allNoTextInputs[i].value == '') {
+                alert ('ERROR: Please enter all text boxes.')
+            } else {
+                aliensFakeForm.style.display = 'none'
+                aliensFakeImgs.style.display = 'contents'
+                document.querySelector('#madlib-fake').innerHTML = noText
+            }
+        }
+    }
 }())
